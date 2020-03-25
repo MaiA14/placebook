45e2609639f0ab56d8eb5c551cb3e8fba834bcbb
Mai Aloni <maiaa1993@gmail.com>
2020-03-25 15:15:38 +0200

    form-hook, input, updatePlace functionality 15:15

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 import Users from './user/pages/Users';
-import NewPlace from './places/pages/NewPlace';
+import NewPlace from './places/pages/NewPlace/NewPlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation/MainNavigation'
 import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace/UpdatePlace';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <Route component={Users} path="/" exact></Route>
           <Route component={UserPlaces} path="/:userId/places" exact></Route>
           <Route component={NewPlace} path="/places/new" exact></Route>
+          <Route component={UpdatePlace} path="/places/:placeId" exact></Route>
           <Redirect to="/"></Redirect>
         </Switch>
       </main>
--- src/places/pages/NewPlace.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NewPlace = () => {
-    return <h1>New place works</h1>
-}
-
-export default NewPlace;
